bd83a3b808452cfc91ba316b41c0bf5cfeb1cbc6
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-10 09:55:06 +0530

    advtask html

---
+++ advTask1.js
@@ -0,0 +1,42 @@
+const arr=[];
+ function add()
+ {
+   let userid= document.getElementById('uid').value;
+
+   let username= document.getElementById('username').value;
+
+   let salary= document.getElementById('salary').value;
+
+var obj={userid:userid,username:username,salary:salary};
+arr.push(obj);
+ 
+populate();
+
+}
+function populate(){
+
+var text="";
+text = "<table id='tbl2'><tr><th>ID</th><th>Name</th><th> salary</th><th>Action</th></tr>"
+for(i=0;i<arr.length;i++){
+    text+="<tr><td>"+arr[i].userid+"</td><td>"+arr[i].username+"</td><td>"+arr[i].salary+"</td><td><button onclick='deleteElement(this)'>delete</button></td></tr>"
+}
+document.getElementById('tbleData').innerHTML=text;
+text="</table>"
+}
+
+
+
+function deleteElement(args)
+{
+    tr=args.parentNode.parentNode;
+   
+    let index=tr.rowIndex;
+      
+  arr.splice(index-1,1);
+  populate();
+
+
+
+}
+
+
